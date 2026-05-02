1c58f45daa16f8c859b18f273e90eb203b219221
Z User <z@container>
2026-05-02 04:10:25 +0000

    cleanup: remove rogue Java files that shadowed Android framework classes (android.util.Log, android.content.Context)

--- android/content/Context.java
+++
@@ -1,7 +0,0 @@
-package android.content;
-
-public class Context {
-    public Context getApplicationContext() {
-        return this;
-    }
-}
--- com/domwood/nexusai/ai/AICore.java
+++
@@ -1,5 +0,0 @@
-package com.domwood.nexusai.ai;
-
-public class AICore {
-    // Stub class for non-Android Java compilation.
-}
